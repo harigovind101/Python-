b23941dd131fc8887b4246dc5c33e75acdc553ce
Harigovind R <harigovindr101@gmail.com>
2026-06-21 15:52:02 +0530

    Add user input functionality
    Updated the script to include basic user interaction using the input() function. Added examples of asking for a user's name and favorite color.

---
+++ 2Receiving Input.py
@@ -0,0 +1,18 @@
+name = input("what is your name? ")
+print("hello",name)
+"""
+output is
+    what is your name? hari
+    hello hari
+"""
+ 
+
+ #ask person name and fav color and print a meassage like hari like black
+person_name = input("what is your name?: ")
+fav_color = input("your favourate colour:" )
+print (person_name,"likes",fav_color)
+"""
+output is
+    what is your name?: hari
+    your favourate colour:black
+    hari likes black"""
