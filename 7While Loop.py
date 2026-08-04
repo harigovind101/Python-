b8929a7b815cbed5e6a0001408a6a42389032cf3
Harigovind R <harigovindr101@gmail.com>
2026-08-04 13:22:59 +0530

    Add Python while loop and interactive prompt practice

---
+++ 7While Loop.py
@@ -0,0 +1,14 @@
+num = 100
+while num >0:
+    print("hello",num)
+    num = num - 10
+
+num = 100
+while num >0:
+    print(num)
+    num = num//5
+
+command =""
+while command != "Quit":
+    command = input("-->")
+    print("alin",command)
